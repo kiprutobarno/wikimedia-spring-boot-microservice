8406c15f3fb0a32143d2d10022c4c9d5106347ee
ywalakamar <barxwells@gmail.com>
2023-10-28 01:29:21 +0300

    chore: setup multi-module project

---
+++ src/main/java/com/tuts/realworldspringbootkafka/RealworldSpringBootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.tuts.realworldspringbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldSpringBootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealworldSpringBootKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuts/realworldspringbootkafka/RealworldSpringBootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuts.realworldspringbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldSpringBootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
